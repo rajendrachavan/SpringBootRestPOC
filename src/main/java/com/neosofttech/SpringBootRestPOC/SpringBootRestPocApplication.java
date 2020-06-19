39bb1c70d7c5daafdfb46e5de87152ae4e4015a2
rajendrachavan <rajendrachavan.jvm@gmail.com>
2020-06-19 12:34:00 +0530

    Initial Commit through local Directory.

---
+++ src/main/java/com/neosofttech/SpringBootRestPOC/SpringBootRestPocApplication.java
@@ -0,0 +1,13 @@
+package com.neosofttech.SpringBootRestPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neosofttech/SpringBootRestPOC/SpringBootRestPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neosofttech.SpringBootRestPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
